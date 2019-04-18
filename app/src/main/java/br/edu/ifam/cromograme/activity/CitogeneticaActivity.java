201015e44f5d5c8bd9a423ccca05ba4268732236
Gladson <gladsonsouza@live.com>
2019-04-18 19:45:50 -0400

    Activitys de base adicionadas.

--- app/src/main/java/br/edu/ifam/cromograme/activity/CitogeneticaActivity.java
+++ app/src/main/java/br/edu/ifam/cromograme/activity/CitogeneticaActivity.java
@@ -1,7 +1,9 @@
-package br.edu.ifam.cromograme;
+package br.edu.ifam.cromograme.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.edu.ifam.cromograme.R;
 
 public class CitogeneticaActivity extends AppCompatActivity {
 
