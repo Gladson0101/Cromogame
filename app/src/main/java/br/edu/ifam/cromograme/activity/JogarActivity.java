27ea5c91f097527cf6f31678ce04603397ccf62e
Gladson <gladsonsouza@live.com>
2019-05-13 13:16:15 -0400

    Jogo de Divisão Celular Remodulado

--- app/src/main/java/br/edu/ifam/cromograme/activity/JogarActivity.java
+++ app/src/main/java/br/edu/ifam/cromograme/activity/JogarActivity.java
@@ -1,5 +1,6 @@
 package br.edu.ifam.cromograme.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,6 +26,10 @@ public class JogarActivity extends AppCompatActivity {
     }
 
     public void jogoDivisaoCelular(View view) {
+        startActivity(new Intent(getApplicationContext(), JogoDivisaoCelularActivity.class));
+    }
+
+    public void jogarDoencas(View view) {
 
     }
 }
---
+++ app/src/main/java/br/edu/ifam/cromograme/activity/JogoDivisaoCelularActivity.java
@@ -0,0 +1,121 @@
+package br.edu.ifam.cromograme.activity;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import br.edu.ifam.cromograme.R;
+import br.edu.ifam.cromograme.fragment.DivCelNivel10Fragment;
+import br.edu.ifam.cromograme.fragment.DivCelNivel1Fragment;
+import br.edu.ifam.cromograme.fragment.DivCelNivel2Fragment;
+import br.edu.ifam.cromograme.fragment.DivCelNivel3Fragment;
+import br.edu.ifam.cromograme.fragment.DivCelNivel4Fragment;
+import br.edu.ifam.cromograme.fragment.DivCelNivel5Fragment;
+import br.edu.ifam.cromograme.fragment.DivCelNivel6Fragment;
+import br.edu.ifam.cromograme.fragment.DivCelNivel7Fragment;
+import br.edu.ifam.cromograme.fragment.DivCelNivel8Fragment;
+import br.edu.ifam.cromograme.fragment.DivCelNivel9Fragment;
+
+public class JogoDivisaoCelularActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button button;
+    private int index = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jogo_divisao_celular);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.frameLayoutJogoDivisaoCelular, new DivCelNivel1Fragment());
+        transaction.commit();
+
+        textView = findViewById(R.id.textViewQuestaoDivisaoCelular);
+        textView.setText("Questao: " + index + "/10");
+
+        button = findViewById(R.id.buttonJogoDivisaoCelularProximo);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (index) {
+                    case 1: {
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frameLayoutJogoDivisaoCelular, new DivCelNivel2Fragment());
+                        transaction.commit();
+                        index++;
+                        textView.setText("Questao: " + index + "/10");
+                    }
+                    break;
+                    case 2: {
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frameLayoutJogoDivisaoCelular, new DivCelNivel3Fragment());
+                        transaction.commit();
+                        index++;
+                        textView.setText("Questao: " + index + "/10");
+                    }
+                    break;
+                    case 3: {
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frameLayoutJogoDivisaoCelular, new DivCelNivel4Fragment());
+                        transaction.commit();
+                        index++;
+                        textView.setText("Questao: " + index + "/10");
+                    }
+                    break;
+                    case 4: {
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frameLayoutJogoDivisaoCelular, new DivCelNivel5Fragment());
+                        transaction.commit();
+                        index++;
+                        textView.setText("Questao: " + index + "/10");
+                    }
+                    break;
+                    case 5: {
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frameLayoutJogoDivisaoCelular, new DivCelNivel6Fragment());
+                        transaction.commit();
+                        index++;
+                        textView.setText("Questao: " + index + "/10");
+                    }
+                    break;
+                    case 6: {
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frameLayoutJogoDivisaoCelular, new DivCelNivel7Fragment());
+                        transaction.commit();
+                        index++;
+                        textView.setText("Questao: " + index + "/10");
+                    }
+                    break;
+                    case 7: {
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frameLayoutJogoDivisaoCelular, new DivCelNivel8Fragment());
+                        transaction.commit();
+                        index++;
+                        textView.setText("Questao: " + index + "/10");
+                    }
+                    break;
+                    case 8: {
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frameLayoutJogoDivisaoCelular, new DivCelNivel9Fragment());
+                        transaction.commit();
+                        index++;
+                        textView.setText("Questao: " + index + "/10");
+                    }
+                    break;
+                    case 9: {
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frameLayoutJogoDivisaoCelular, new DivCelNivel10Fragment());
+                        transaction.commit();
+                        index++;
+                        textView.setText("Questao: " + index + "/10");
+                    }
+                    break;
+                }
+            }
+        });
+    }
+}
